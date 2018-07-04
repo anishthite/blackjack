0843cb06e6fbc92acab33fea83f9088fbcffd786
Anish Thite <anishthite@live.com>
2018-07-03 23:09:04 -0400

    m

--- src/deck.py
+++ src/deck.py
@@ -4,33 +4,33 @@ Created on Jul 1, 2018
 @author: anish
 '''
 from random import randint
+from collections import deque
 class Card:
 	def __init__(self, number):
 		self.number = number   
-	def number(self):
-		print (self.number)
-
+	def faceup(self):
+		print ( "[" + self.number + "]")
+	def facedown(self):
+		print ("[X]")
 class Deck:
 	def shuffle(self):
 		for x in range(52):
-			posa = randint(0,52)
-			posb = randint(0,52)
+			posa = randint(0,51)
+			posb = randint(0,51)
 			carriercard = self.deck[posa]
 			self.deck[posa] = self.deck[posb]
 			self.deck[posb] = carriercard	 
 	def __init__(self):
-		self.deck = []
+		self.deck = deque([])
 		DeckNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K"]
 		for x in range(4):
 			for value in DeckNumbers:
 				newcard = Card(value)
 				self.deck.append(newcard)
-				self.deck.shuffle()
+		self.shuffle()
 	def cards(self):
 		for card in self.deck:
 			print(card.number + ",")
 	def draw(self, number, location):
 		for x in range(number):
 			location.append(self.deck.popleft())
-
-
--- src/gameplay.py
+++ src/gameplay.py
@@ -4,25 +4,50 @@ Created on Jul 1, 2018
 @author: anish
 '''
 from deck import Deck as Deck
-import player
-global gameLive, deck, player
+import player as pl
 def setup():
-    deck = Deck()
-    player = player.Player(1000)
-    dealer = player.Dealer()
-    gameLive = True
+	global deck, player, dealer
+	deck = Deck()
+	player = pl.Player(1000)
+	dealer = pl.Dealer()
+def bet():
+	global betAmount
+	#print ("You have bet " + betAmount)
+	betAmount = input("How much would you like to bet?")
+	
+def play():
+	global player
+	playType = ""
+	while ((playType != "hit") or (playType != "stand")):  
+		playType  = input("Would you like to hit, or stand").lower()
+	if (playType == "hit"):
+		deck.draw(1, player.hand)
+		for card in player.hand:
+			card.faceup()
+	if (playType == "stand"): 
+		return
 def main():
-    #setup
-    setup()
-    #loop
-    print('Shuffling cards (1 deck)')
-    print('Player Balance: ' + player.cash)
-    while (gameLive == True):
-        #dealer puts your cards
-        deck.draw(2,player.hand)
-     
-    #dealer puts their cards
-    #you bet
-    #you decide to get one more card
-    #loop
-    #dealer takes the cards as they add up to at least 17
+	#setup
+	setup()
+	#loop
+	print('Shuffling cards (1 deck)')
+	gameLive = True
+	while (gameLive == True):
+		#dealer puts your cards
+		print('Player Balance: ' + str(player.cash))
+		betAmount = 0
+		deck.draw(2,player.hand)
+		for card in player.hand:
+			card.faceup()
+	#dealer puts their cards
+		deck.draw(1, dealer.hand)
+		card.faceup()	
+		deck.draw(1, dealer.hand)
+		card.facedown()
+	#you bet
+		bet()
+	#you decide to get one more card
+		play()
+	#loop
+	#dealer takes the cards as they add up to at least 17
+main()
